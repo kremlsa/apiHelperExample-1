db21c5a5f9798c6c8acf81423f4736f0b5001d84
kremlsa <55805583+kremlsa@users.noreply.github.com>
2021-04-09 09:58:22 +0700

    Update JUnitTestBase.java

--- src/test/java/apiHelperExample/JUnitTestBase.java
+++ src/test/java/apiHelperExample/JUnitTestBase.java
@@ -9,6 +9,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import java.util.concurrent.TimeUnit;
 
